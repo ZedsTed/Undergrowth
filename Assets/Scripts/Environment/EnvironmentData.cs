2861b0c48a96db22da6f1a7c7fd4813076552f30
zedsted <zedsted@gmail.com>
2019-07-10 11:52:09 +0100

    Added environment data singleton to track the data for sun, weather etc.

---
+++ Assets/Scripts/Environment/EnvironmentData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Stores the data for the weather, sun, etc.
+/// </summary>
+public class EnvironmentData : Singleton<EnvironmentData>
+{
+    [SerializeField]
+    protected SunCycle Sun;
+    
+    public float SunIntensity { get { return Sun.sun.intensity; } }
+
+}
