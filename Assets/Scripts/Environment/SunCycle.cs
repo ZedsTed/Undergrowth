b61382d6f9306bfb0b94aa3187082fe8fc8ec5c7
zedsted <zedsted@gmail.com>
2019-07-11 16:45:22 +0100

    Added docs for suncycle

--- Assets/Scripts/Environment/SunCycle.cs
+++ Assets/Scripts/Environment/SunCycle.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using static MathsUtils;
 
+/// <summary>
+/// Currently works but not optimally, lerping makes it jerk and we ideally want to track it better like the following:
+/// https://wiki.unity3d.com/index.php/DayNightController
+/// </summary>
 public class SunCycle : MonoBehaviour
 {
     public Light sun;
     
 
     protected Quaternion qMidday = new Quaternion(0.8251674f, 0f, 0f, 0.5648884f);
-    protected Quaternion qMidnight = new Quaternion(0f, 0.5648882f, 0.8251675f, 0f);
+    protected Quaternion qNextDay = new Quaternion(-0.8251674f, 0f, 0f, -0.5648884f);
 
     protected Quaternion sunRotation = Quaternion.identity;
 
@@ -30,22 +34,27 @@ public class SunCycle : MonoBehaviour
     {
         float percHalfDay = 0f;
 
-        // If we're more than halfway through the day.
-        if (GameTime.Instance.CurrentMinuteAndSeconds > (GameTime.Instance.MinutesInDay / 2))
-        {
-            // Figure out how far we are past midday, so we remove half a day and see how far we are through it
-            percHalfDay = ((GameTime.Instance.CurrentMinuteAndSeconds - (GameTime.Instance.MinutesInDay / 2)) / (GameTime.Instance.MinutesInDay / 2));        
+        // Figure out how far we are past midday, so we remove half a day and see how far we are through it
+        percHalfDay = ((GameTime.Instance.CurrentMinuteAndSeconds - (GameTime.Instance.MinutesInDay / 2)) / (GameTime.Instance.MinutesInDay / 2));
 
-            transform.rotation = Quaternion.Lerp(qMidday, qMidnight, percHalfDay);
-        }
-        else
-        {   // If we're less than halfway through the day
-            // Figure out how far we are past midday, so we remove half a day and see how far we are through it
-            percHalfDay = (GameTime.Instance.CurrentMinuteAndSeconds
-                            / (GameTime.Instance.MinutesInDay / 2));
+        transform.rotation = Quaternion.Lerp(qMidday, qNextDay, percHalfDay);
 
-            transform.rotation = Quaternion.Lerp(qMidnight, qMidday, percHalfDay);
-        }
+        //// If we're more than halfway through the day.
+        //if (GameTime.Instance.CurrentMinuteAndSeconds > (GameTime.Instance.MinutesInDay / 2))
+        //{
+        //    // Figure out how far we are past midday, so we remove half a day and see how far we are through it
+        //    percHalfDay = ((GameTime.Instance.CurrentMinuteAndSeconds - (GameTime.Instance.MinutesInDay / 2)) / (GameTime.Instance.MinutesInDay / 2));        
+
+        //    transform.rotation = Quaternion.Lerp(qMidday, qNextDay, percHalfDay);
+        //}
+        //else
+        //{   // If we're less than halfway through the day
+        //    // Figure out how far we are past midday, so we remove half a day and see how far we are through it
+        //    percHalfDay = (GameTime.Instance.CurrentMinuteAndSeconds
+        //                    / (GameTime.Instance.MinutesInDay / 2));
+
+        //    transform.rotation = Quaternion.Lerp(qNextDay, qMidday, percHalfDay);
+        //}
     }
 
     /// <summary>
