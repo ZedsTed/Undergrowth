a405dc41296b22c5c2f525b4171f111655bc598e
zedsted <zedsted@gmail.com>
2019-07-11 21:07:18 +0100

    Changed tools buttons to toggles, added toggle group. Fixed incorrect pivot and anchor for the tools panel, is now pivoted and anchored to the bottom right.

---
+++ Assets/Scripts/UI/ToolsManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolsManager : MonoBehaviour
+{
+    public Toggle ContainerToggle;
+    public Toggle LandscapingToggle;
+    public Toggle PlantToggle;
+    public Toggle WaterToggle;
+    public Toggle RemoveToggle;
+
+
+    protected void Start()
+    {
+
+    }
+
+    protected void Update()
+    {
+
+    }
+}
